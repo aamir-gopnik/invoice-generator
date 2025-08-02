85bdcd3b7d7a141888fb2acb948ccf0d97b2795b
aamir-gopnik <aamir.gopnik@gmail.com>
2025-08-02 13:18:10 +0530

    Resolved frontend issues

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Customer, Product, Invoice } from '../types';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'http://localhost:8888/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
